dc4a9d17b0f64b9245f8dc2b4aa0008b2ed90aaa
anzumpolok <mdsafaatanzumpolok@gmail.com>
2026-03-07 03:17:26 +0600

    commit demo email update

--- src/util/api.js
+++ src/util/api.js
@@ -2,17 +2,21 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://shop-pos-server-production.up.railway.app",
+
+
+
+
   headers: {
     "Content-Type": 'application/json'
   }
 })
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("jwt");
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("jwt");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
 
-  return config;
-});
+//   return config;
+// });
 export default api;
