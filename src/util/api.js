4b48231fa6a3671659a51825398d062b36c768af
anzumpolok <mdsafaatanzumpolok@gmail.com>
2026-03-06 16:14:22 +0600

    update api2 page

--- src/util/api.js
+++ src/util/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://shop-pos-server-production.up.railway.app/",
+  baseURL: "https://shop-pos-server-production.up.railway.app",
   headers: {
     "Content-Type": 'application/json'
   }
